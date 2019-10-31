d2e5db75afbb48227e421d551c69c1acb1700b3a
toratt <Attila.Torjak@kartverket.no>
2019-10-31 15:45:32 +0100

    Add lat, lon, zoom and crs params.

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-import 'regenerator-runtime/runtime'
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { MapComponent } from 'r_map'
+import 'regenerator-runtime/runtime';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { MapComponent } from 'r_map';
 
-export const map = (mapItems, element) => {
-  ReactDOM.render(<MapComponent services={mapItems} />, element)
-}
+export const map = (params, element) => {
+    ReactDOM.render(<MapComponent zoom={params.zoom} lat={params.lat} lon={params.lon} crs={params.crs} services={params.mapItems} />, element);
+};
