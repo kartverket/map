47c1803fa5ae68581d370c35fa9cbe78a7aa8db3
toratt <Attila.Torjak@kartverket.no>
2020-02-26 14:36:26 +0100

    Add legend

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 import 'regenerator-runtime/runtime';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { MapComponent } from 'r_map';
+import { MapComponent, Legend } from 'r_map';
 
 export const map = (params, element) => {
     ReactDOM.render(<MapComponent zoom={params.zoom} lat={params.lat} lon={params.lon} crs={params.crs} services={params.mapItems} />, element);
+};
+
+export const legend = (url, element) => {
+    ReactDOM.render(<Legend legendUrl={url} />, element);
 };
